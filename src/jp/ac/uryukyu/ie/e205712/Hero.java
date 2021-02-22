2d03d21bdef0fa6c7d03a53f28ff224de65a0b9f
Ryo Sakihara <e205712@ie.u-ryukyu.ac.jp>
2021-02-22 10:54:42 +0900

    javaDocを記載したfile

--- src/jp/ac/uryukyu/ie/e205712/Hero.java
+++ src/jp/ac/uryukyu/ie/e205712/Hero.java
@@ -1,54 +1,55 @@
 //package jp.ac.uryukyu.ie.e205712;
 
-public class Hero {
-    /**
- * ヒーロークラス。
- *  String name; //敵の名前
- *  int hitPoint; //敵のHP
- *  int attack; //敵の攻撃力
- *  boolean dead; //敵の生死状態。true=死亡。
- * Created by tnal on 2016/11/13.
+/**
+ * ヒーロークラス。 String name; //敵の名前 int hitPoint; //敵のHP int attack; //敵の攻撃力 boolean
+ * dead; //敵の生死状態。true=死亡。 Created by tnal on 2016/11/13.
  */
+public class Hero {
     private String name;
     private int hitPoint;
     private int attack;
     private boolean dead;
 
-    String getName(){
-    /**敵の名前の中継
-     * 敵の名前を返す
+    /**
+     * 敵の名前の中継 敵の名前を返す。
+     * @return String name;
      */
+    String getName() {
         return name;
     }
 
-    public int getHitPoint(){
-    /**敵のHPの中継
-     * 敵のHPを返す
+    /**
+     * 敵のHPの中継 敵のHPを返す。
+     * @return int hitPoint;
      */
+    public int getHitPoint() {
         return hitPoint;
     }
 
-    public int getAttack(){
-    /**敵の攻撃力の中継
-     * 敵の攻撃力を返す
+    /**
+     * 敵の攻撃力の中継 敵の攻撃力を返す。
+     * @return int attack;
      */
+    public int getAttack() {
         return attack;
     }
 
-    public boolean getDead(){
-    /**敵の生死状態の中継
-     * 敵の生死状態を返す
+    /**
+     * 敵の生死状態の中継 敵の生死状態を返す。
+     * @return boolean dead;
      */
+    public boolean getDead() {
         return dead;
     }
 
     /**
      * コンストラクタ。名前、最大HP、攻撃力を指定する。
-     * @param name ヒーロー名
+     * 
+     * @param name      ヒーロー名
      * @param maximumHP ヒーローのHP
-     * @param attack ヒーローの攻撃力
+     * @param attack    ヒーローの攻撃力
      */
-    public Hero (String name, int maximumHP, int attack) {
+    public Hero(String name, int maximumHP, int attack) {
         this.name = name;
         hitPoint = maximumHP;
         this.attack = attack;
@@ -57,24 +58,24 @@ public class Hero {
     }
 
     /**
-     * Enemyへ攻撃するメソッド。
-     * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
+     * Enemyへ攻撃するメソッド。 attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
+     * 
      * @param e 攻撃対象
      */
-    public void attack(Enemy e){
-        int damage = (int)(Math.random() * attack);
+    public void attack(Enemy e) {
+        int damage = (int) (Math.random() * attack);
         System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, e.getName(), damage);
         e.wounded(damage);
     }
 
     /**
-     * 自身へ攻撃されたときのダメージ処理をするメソッド。
-     * 指定されたダメージを hitPoint から引き、死亡判定を行う。
+     * 自身へ攻撃されたときのダメージ処理をするメソッド。 指定されたダメージを hitPoint から引き、死亡判定を行う。
+     * 
      * @param damage 受けたダメージ
      */
-    public void wounded(int damage){
+    public void wounded(int damage) {
         hitPoint -= damage;
-        if( hitPoint < 0 ) {
+        if (hitPoint < 0) {
             dead = true;
             System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", name);
         }
